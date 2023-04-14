77222bb9f82cea95cf7e2a331ba91d6e1c1c30d5
Hadeer Khaled <hadeer.awad02@eng-st.cu.edu.eg>
2023-04-14 08:11:00 +0200

    Final Version

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let progBar = document.querySelector(".progress-bar")
 var currentTimeSpan = document.querySelector(".current")
 var finalTimeSpan = document.querySelector(".final")
 
-var index = 1
+var index = 2
 
 // Create PlayList
 function createSongsList() {
